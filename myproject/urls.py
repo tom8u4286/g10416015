e7dab56a1b6418461a55c3a3998eb408aa851429
Tomtom <tom8u4286@gmail.com>
2016-12-08 11:12:33 +0800

    index test

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^$', myapp.views.index),
+	url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
 ]
